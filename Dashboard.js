b882cd60fe4576e0b972ed4a99b36076c70e1029
Rahul Raikar <46755041+Rahul054@users.noreply.github.com>
2019-01-23 10:18:54 +0530

    Update Dashboard.js

--- Dashboard.js
+++ Dashboard.js
@@ -32,7 +32,6 @@ $(function () {
       }
 
     });
-
     var slideNavDropdown = $('.side-nav-dropdown');
 
     $('.side-nav .categories li').on('click', function () {
@@ -62,12 +61,14 @@ $(function () {
     });
 
   }());
-
   var chart = document.getElementById('myChart');
-  Chart.defaults.global.animation.duration = 2000;          Chart.defaults.global.title.text = "Chart";
-  Chart.defaults.global.title.position = 'bottom'; 
-  Chart.defaults.global.defaultFontSize = 10; 
-  Chart.defaults.global.tooltips.borderColor = 'white';
+  Chart.defaults.global.animation.duration = 2000; // Animation duration
+  Chart.defaults.global.title.display = false; // Remove title
+  Chart.defaults.global.title.text = "Chart"; // Title
+  Chart.defaults.global.title.position = 'bottom'; // Title position
+  Chart.defaults.global.defaultFontColor = '#999'; // Font color
+  Chart.defaults.global.defaultFontSize = 10; // Font size 
+  Chart.defaults.global.tooltips.borderColor = 'white'; // Tooltips border color
   Chart.defaults.global.legend.labels.padding = 0;
   Chart.defaults.scale.ticks.beginAtZero = true;
   Chart.defaults.scale.gridLines.zeroLineColor = 'rgba(255, 255, 255, 0.1)';
@@ -94,6 +95,7 @@ $(function () {
         lineTension: 0,
         startAngle: 2,
         data: [20, 40, 20, 45, 25, 60],
+        // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
         backgroundColor: "transparent",
         pointBorderColor: "#ff6384",
         borderColor: '#ff6384',
@@ -102,7 +104,9 @@ $(function () {
       }]
     },
   });
-  
+  //  Chart ( 2 )
+
+
   var Chart2 = document.getElementById('myChart2').getContext('2d');
   var chart = new Chart(Chart2, {
     type: 'line',
@@ -123,6 +127,7 @@ $(function () {
         lineTension: .4,
         startAngle: 2,
         data: [20, 14, 20, 25, 10, 15, 25, 10],
+        // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
         backgroundColor: "transparent",
         pointBorderColor: "#4bc0c0",
         borderColor: '#4bc0c0',
@@ -134,6 +139,7 @@ $(function () {
         lineTension: .4,
         startAngle: 2,
         data: [40, 20, 5, 10, 30, 15, 15, 10],
+        // , '#ff6384', '#4bc0c0', '#ffcd56', '#457ba1'
         backgroundColor: "transparent",
         pointBorderColor: "#ffcd56",
         borderColor: '#ffcd56',
@@ -142,6 +148,7 @@ $(function () {
       }]
     },
 
+    // Configuration options
     options: {
       title: {
         display: false
